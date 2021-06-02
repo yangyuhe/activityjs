04a8ee1489ea3827ef6920b36ec21ad5f93ad396
hexiang1 <hexiang1@xiaomi.com>
2021-06-02 14:41:41 +0800

    增加hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+/**
+ * 绑定model的值到组件
+ * @param propertyMap 通过{key:()=>any}的形式指定可响应式的属性, key是传递给组件的属性名，值是()=>any执行的结果，值也可以直接是一个非函数的任意值
+ * @param Com 被连接的组件
+ * @returns 返回新的组件
+ */
 export function connect(
   propertyMap: { [key: string]: (() => any) | any },
   Com: any
 ): any {
+  //存储所有需要监听这些值变化的组件的setState方法
   let callbacks = [];
+  //监听propertyMap值的变化，当propertyMap中的某几项的值发生变化时会通知到回调
   notify(propertyMap, (values) => {
     callbacks.forEach((cb) => cb(values));
   });
+  //这是实际订阅了model数据的组件
   return class Activity extends React.PureComponent {
     private _setState;
     constructor(p) {
@@ -19,13 +27,10 @@ export function connect(
       });
       this.state = props;
 
-      let queue = new EventQueue((states) => {
-        this.setState(states);
-      });
       this._setState = (state) => {
-        // queue.enter(state);
         this.setState(state);
       };
+      //当数据发生变化时通知组件设置新的state
       callbacks.push(this._setState);
     }
 
@@ -38,6 +43,7 @@ export function connect(
     }
   };
 }
+/**通过代理实现对数据变换的监听 */
 export function watch<T>(model: T) {
   if (watchedModels.includes(model)) {
     return model;
@@ -49,6 +55,7 @@ export function watch<T>(model: T) {
       let value = model[key];
       Object.defineProperty(model, key, {
         get() {
+          //搜集propertyMap中和当前key相关的值
           if (curContext) {
             const { watching, curKey, watchedMap, propertyMap } = curContext;
             if (watching && curKey) {
@@ -67,7 +74,7 @@ export function watch<T>(model: T) {
         set(val) {
           if (val !== value) {
             value = val;
-
+            //当model的数据更新后重新计算propertyMap中受影响的值并通知到回调
             contexts.forEach((context) => {
               const { watchedMap } = context;
               watchedMap.forEach((item) => {
@@ -112,6 +119,7 @@ export function watch<T>(model: T) {
   });
   return model;
 }
+/**执行一次watch的上下文记录 */
 interface Context {
   propertyMap: { [key: string]: () => any };
   callback: (values) => void;
@@ -122,9 +130,18 @@ interface Context {
   /**上一次映射的属性集合 */
   oldValues: { [key: string]: any };
 }
-const contexts: Context[] = [];
+/**所有的watch的上下文 */
+let contexts: Context[] = [];
+
 let curContext: Context = null;
+/**activityjs正在监听的所有的model的集合 */
 let watchedModels = [];
+/**
+ * 监听这组{key:()=>any}的值，当model数据更新导致其中某几项更新事通知到callback
+ * @param propertyMap
+ * @param callback
+ * @returns
+ */
 function notify(propertyMap, callback) {
   const context: Context = {
     propertyMap,
@@ -135,9 +152,12 @@ function notify(propertyMap, callback) {
     oldValues: {},
   };
   contexts.push(context);
-  watchingContext(context);
+  startWatch(context);
+  return () => {
+    contexts = contexts.filter((item) => item != context);
+  };
 }
-function watchingContext(context: Context) {
+function startWatch(context: Context) {
   context.watching = true;
 
   let props = {};
@@ -163,27 +183,25 @@ function evalFn(fn) {
     return fn;
   }
 }
-
-class EventQueue {
-  private queue = [];
-  private callback = null;
-  constructor(callback) {
-    this.callback = callback;
-  }
-  enter(state) {
-    if (this.queue.length === 0) {
-      Promise.resolve().then(() => {
-        this.consume();
-      });
-    }
-    this.queue.push(state);
-  }
-  private consume() {
-    let states = {};
-    this.queue.forEach((state) => {
-      Object.assign(states, state);
-    });
-    this.queue = [];
-    this.callback(states);
-  }
+/**
+ * hook支持，fn的计算结果将作为值返回
+ * @param fn
+ * @returns fn的计算结果
+ */
+export function useActivity(fn) {
+  const [state, setState] = useState(() => {
+    fn();
+  });
+  useEffect(() => {
+    let unsub = notify(
+      {
+        val: () => fn(),
+      },
+      ({ val }) => {
+        setState(() => val);
+      }
+    );
+    return unsub;
+  }, []);
+  return state;
 }
--- test/main.js
+++ test/main.js
@@ -1,161 +1,33 @@
-/* eslint-disable react/prop-types */
-import React, { useEffect, useState } from "react";
-import { connect, watch } from "../src/index";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
-class Model {
-  constructor(id) {
-    this.id = id;
-    this.age = 0;
-    this.person = { name: "hexiang" };
-    this.submodel = null;
-    if (id === "init") this.submodel = watch(new Model("second"));
-  }
-  setName = () => {
-    this.person = { name: "lix" };
-  };
+import { watch, useActivity } from "../src/index";
 
-  add = () => {
-    this.age++;
-    if (this.age % 5 === 0) {
-      if (!this.submodel)
-        this.submodel = watch(
-          new Model(this.id === "init" ? "second" : "third")
-        );
-      else this.submodel = null;
-    }
-  };
+let model1 = watch({
+  count: 0,
+});
+function increase() {
+  model1.count++;
+}
+function decrease() {
+  model1.count--;
 }
 
-const model = watch(new Model("init"));
-
-window.__model = model;
-const FirstLevelCounter = connect(
-  {
-    visible: true,
-    myage: () => model.age,
-    myadd: () => model.add,
-    name: () => model.person.name,
-    changename: () => model.setName,
-  },
-  Counter
-);
-const SecondLevelCounter = connect(
-  {
-    visible: () => !!model.submodel,
-    myage: () => model.submodel.age,
-    myadd: () => model.submodel.add,
-    sonid: () => model.submodel.submodel && model.submodel.submodel.id,
-    id: () => model.submodel.id,
-    changename: () => model.submodel.setName,
-    name: () => model.submodel.person.name,
-  },
-  Counter
-);
-const ThirdLevelCounter = connect(
-  {
-    visible: () => !!model.submodel.submodel,
-    myage: () => model.submodel.submodel.age,
-    myadd: () => model.submodel.submodel.add,
-    id: () => model.submodel.submodel.id,
-  },
-  Counter
-);
-
-function Counter(props) {
-  useEffect(() => {
-    console.log("id:", props.id, " create");
-    return () => {
-      console.log("id:", props.id, " destroy");
-    };
-  }, []);
-  console.log("render...");
-  if (props.visible)
-    return (
-      <>
-        {props.sonid ? <h1>sonid:{props.sonid}</h1> : null}
-        {props.title ? <h1>{props.title}</h1> : null}
-        <h1>{props.myage}</h1>
-        {props.count ? <h2>{props.count}</h2> : null}
-        <button onClick={props.myadd}>add</button>
-        <button onClick={props.changename}>change name</button>
-        <h3>{props.name}</h3>
-      </>
-    );
-  return null;
+function Counter1() {
+  const count = useActivity(() => model1.count);
+  return <h1>{count}</h1>;
 }
-class Model1 {
-  constructor() {
-    this.time = new Date().toLocaleString();
-    // this.timmer = setInterval(() => {
-    //   this.time = new Date().toLocaleString();
-    // }, 1000);
-  }
-}
-let model1 = watch(new Model1());
-class Model2 {
-  constructor() {
-    this.time = new Date().toISOString();
-    // this.timmer = setInterval(() => {
-    //   this.time = new Date().toISOString();
-    // }, 1000);
-    this.person = { name: "小毛" };
-    this.foo = "foo";
-  }
-  setName(str) {
-    setTimeout(() => {
-      this.foo = "bar";
-      this.person = { name: str };
-    }, 0);
-  }
-}
-let model2 = watch(new Model2());
-function Clock(props) {
-  console.log("render clock", props);
-  return (
-    <div>
-      <h1>美国:{props.american}</h1>
-      <h1>中国:{props.china}</h1>
-      <h2>
-        secondModel:{props.secondModel},id:{props.id}
-      </h2>
-      <h2>person name:{props.name}</h2>
-      <button
-        onClick={() => {
-          model2.setName("三毛");
-        }}
-      >
-        change name
-      </button>
-    </div>
-  );
-}
-const WrapClock = connect(
-  {
-    secondModel: () => model.submodel.submodel.age,
-    id: () => model.submodel.submodel.id,
-    american: () => model1.time,
-    china: () => model2.time,
-    name: () => model2.person.name,
-    foo: () => model2.foo,
-  },
-  Clock
-);
 
 function App() {
-  const [count, setCount] = useState(0);
-  const test = () => {
-    setCount(count + 1);
-  };
+  let count = useActivity(() => model1.count);
   return (
     <>
-      <h1>测试动态绑定</h1>
-      <FirstLevelCounter title="init" />
-      <SecondLevelCounter title="second" />
-      <ThirdLevelCounter count={count} title="third" />
-      <ThirdLevelCounter count={count} title="fourth" />
-      <button onClick={test}>good</button>
-      <h1>测试绑定多个model</h1>
-      <WrapClock />
+      {Array(count)
+        .fill(0)
+        .map((item, index) => (
+          <Counter1 key={index} />
+        ))}
+      <button onClick={increase}>increase</button>
+      <button onClick={decrease}>decrease</button>
     </>
   );
 }
